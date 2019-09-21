649d46d86721ca9dc8f1824ae27186442a591091
rocket3989 <rocket3989@gmail.com>
2019-09-21 16:31:09 -0500

    fixed tokens

--- grant.js
+++ grant.js
@@ -17,27 +17,29 @@ let _token = hash.access_token;
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 // Replace with your app's client ID, redirect URI and desired scopes
-const clientId = '2102d6bf57714410a8f50dd1ccadc571';
-const redirectUri = 'https://spotify-implicit-grant.glitch.me';
+console.log(window.location.href)
+
+const clientId = '012948ea7d784e819590b1096b416b0d';
+const redirectUri = window.location.href;
 const scopes = [
-  'user-top-read'
+    'user-top-read'
 ];
 
 // If there is no token, redirect to Spotify authorization
 if (!_token) {
-  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+    window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
 }
 
 // Make a call using the token
 $.ajax({
-   url: "https://api.spotify.com/v1/me/top/artists",
-   type: "GET",
-   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
-   success: function(data) { 
-     // Do something with the returned data
-     data.items.map(function(artist) {
-       let item = $('<li>' + artist.name + '</li>');
-       item.appendTo($('#top-artists'));
-     });
-   }
+    url: "https://api.spotify.com/v1/me/top/artists",
+    type: "GET",
+    beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
+    success: function(data) { 
+        // Do something with the returned data
+        data.items.map(function(artist) {
+        let item = $('<li>' + artist.name + '</li>');
+        item.appendTo($('#top-artists'));
+        });
+    }
 });
