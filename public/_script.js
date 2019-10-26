6d8dd0ce6cedc2f6f16a53ea590e1b0ed05bf1c9
Adam Pearce <adampearce@google.com>
2019-10-25 23:13:38 -0400

    song vtable works

--- public/_script.js
+++ public/_script.js
@@ -17,17 +17,22 @@ d3.loadData('tidy.tsv', (err, res) => {
     {str: 'Title', ra: 0, w: 180, val: d => d.song},
     {str: 'Artist', ra: 0, w: 180, val: d => d.artist},
     {str: 'Album', ra: 0, w: 180, val: d => d.album},
-    {str: 'Year', ra: 1, w: 46, val: d => d.year},
+    {str: 'Date', ra: 0, w: 75, val: d => d.date},
   ]
 
   artistTable = initArtistTable()
   songTable = initLongScroll(d3.select('#songs'), songs, songCols)
+
+
+  function renderAll(){
+    songTable.render()
+  }
+
+  renderAll()
+
 })
 
 
-function renderAll(){
-
-}
 
 
 function initArtistTable(){
@@ -97,43 +102,74 @@ function initLongScroll(sel, data, cols){
     headerSel.classed('up', 0).classed('down', 0)
     headerSel.filter(e => e == d).classed(d.notReverse ? 'up' : 'down', 1)
 
-    byArtist = _.sortBy(byArtist, d.val)
-    if (!d.notReverse) byArtist.reverse() 
+    data = _.sortBy(data, d.val)
+    if (!d.notReverse) data.reverse() 
 
-    byArtist.forEach((d, i) => d.i = i)
+    data.forEach((d, i) => d.i = i)
+
+    scroll()
   }
-
 
   //https://blocks.roadtolarissa.com/jasondavies/3689677
   //https://dev.to/adamklein/build-your-own-virtual-scroll-part-i-11ib
   var contSel = sel.append('div.row-container')
-    .on('scroll.longscroll', scroll)
+    .on('scroll.longscroll', render)
   var contNode = contSel.node()
 
-  var rowHeight = 30
+  var rowHeight = 19
   var contHeight = contSel.node().offsetHeight
+  var numRows = 1 + Math.ceil(contHeight/rowHeight)
+  var vData = data.slice(0, numRows) // visable data
 
   var currentSel = contSel
     .append('div').st({height: data.length*rowHeight})
     .append('div.current')
 
-  function scroll(scrollTop){
+  var rowSel = currentSel.appendMany('div.row', d3.range(numRows))
+    .on('click', function(i){
+      var d = vData[i]
+      if (!d) return
+
+      var wasSelected = d.selected
+      data.forEach(e => e.selected = d == e)
+      if (wasSelected) d.selected = false
+
+      render()
+      rowSel
+
+      // TODO play song?
+    })
+
+  var colSel = rowSel.appendMany('div.col', i => cols.map(col => ({col, i})))
+    .st({width: d => d.col.w})
+    .classed('align-right', d => d.col.ra)
+
+
+  function render(){
     var position = Math.floor(contNode.scrollTop/rowHeight)
-    var rows = 1 + Math.ceil(contHeight/rowHeight)
-    var p0 = d3.clamp(0, position, data.length - rows)
-    var p1 = p0 + rows
+    var p0 = d3.clamp(0, position, data.length - numRows)
+    var p1 = p0 + numRows
+    
+    vData = data.slice(p0, p1)
 
     currentSel.st({transform: `translateY(${p0*rowHeight}px)`})
-    var rowSel = currentSel.selectAll('.row')
-      .data(data.slice(p0, p1), d => d.songId)
-
-    rowSel.enter().append('div.row')
-      .text(d => d.song)
-      .st({height: rowHeight, fontSize: 12})
-    rowSel.exit().remove()
-    rowSel.order()
+    rowSel.classed('selected', i => vData[i] && vData[i].selected)
+    colSel
+      .st({opacity: d => vData[d.i] ? 1 : 0})
+      .filter(d => vData[d.i])
+      .text(d => d.col.val(vData[d.i]))
   }
 
-  scroll(0)
-
+  return {render}
 }
+
+
+
+
+
+
+
+
+
+
+
