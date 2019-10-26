106a560187d76b8cd0e90554a57754945ed1082f
Adam Pearce <adampearce@google.com>
2019-10-25 23:50:20 -0400

    all three tables

--- public/_script.js
+++ public/_script.js
@@ -10,8 +10,21 @@ d3.loadData('tidy.tsv', (err, res) => {
   byArtist.forEach(d => d.byAlbum = d3.nestBy(d, d => d.album))
   byAlbum = _.flatten(byArtist.map(d => d.byAlbum))
 
-  songs.forEach(d => d.active = true)
+  songs.forEach(d => d.active = d.searchActive = true)
   byAlbum.forEach(d => d.active = true)
+  byArtist.forEach(d => d.active = true)
+
+  var artistCols = [
+    {str: 'Artist', ra: 0, w: 200, val: d => d.key},
+    {str: 'Albums', ra: 1, w: 80, val: d => d3.sum(d.byAlbum, d => d.active)},
+    {str: 'Tracks', ra: 1, w: 80, val: d => d3.sum(d, d => d.searchActive)},
+  ]
+
+  var albumCols = [
+    {str: 'Album', ra: 0, w: 200, val: d => d.key},
+    {str: 'Date', ra: 0, w: 75, val: d => d[0].date},
+    {str: 'Tracks', ra: 1, w: 80, val: d => d3.sum(d, d => d.searchActive)},
+  ]
 
   var songCols = [
     {str: 'Title', ra: 0, w: 180, val: d => d.song},
@@ -20,95 +33,54 @@ d3.loadData('tidy.tsv', (err, res) => {
     {str: 'Date', ra: 0, w: 75, val: d => d.date},
   ]
 
-  artistTable = initArtistTable()
-  songTable = initLongScroll(d3.select('#songs'), songs, songCols)
-
-
-  function renderAll(){
-    songTable.render()
+  window.table = {
+    artist: initLongScroll('#artists', byArtist, artistCols),
+    album: initLongScroll('#albums', byAlbum, albumCols),
+    song: initLongScroll('#songs', songs, songCols)
   }
-
-  renderAll()
+  d3.values(table).forEach(d => d.render())
 
 })
 
+function filterAll(){
+  songs.forEach(d => {
+    var isArtist = !table.artist.selected || table.artist.selected.key == d.artist
+    var isAlbum = true
+    d.searchActive = true
 
-
-
-function initArtistTable(){
-  var sel = d3.select('#artists').html('')
-
-  var cols = [
-    {str: 'Artist', ra: 0, w: 200, val: d => d.key},
-    {str: 'Albums', ra: 1, w: 80, val: d => d3.sum(d.byAlbum, d => d.active)},
-    {str: 'Tracks', ra: 1, w: 80, val: d => d3.sum(d, d => d.active)},
-  ]
-
-  byArtist.forEach(artist => {
-    artist.cols = cols.map(col => {
-      return {col, artist}
-    })
+    d.active = isArtist && isAlbum && d.searchActive
   })
 
-  var headerSel = sel.append('div.header')
-    .appendMany('div.col', cols)
-    .text(d => d.str)
-    .st({width: d => d.w})
-    .classed('align-right', d => d.ra)
-    .on('click', setSort)
-
-  var rowSel = sel.append('div.row-container')
-    .appendMany('div.row', byArtist)
-    .on('click', d => {
-      var prevActive = d.active
-      byArtist.forEach(e => e.active = d == e)
-      if (prevActive) d.active = false
-      rowSel.classed('active', e => e.active)
-    })
-
-  var colSel = rowSel.appendMany('div.col', d => d.cols)
-    .st({width: d => d.col.w})
-    .classed('align-right', d => d.col.ra)
-    .text(d => d.col.val(d.artist))
-
-
-  function setSort(d){
-    d.notReverse = !d.notReverse
-
-    headerSel.classed('up', 0).classed('down', 0)
-    headerSel.filter(e => e == d).classed(d.notReverse ? 'up' : 'down', 1)
-
-    byArtist = _.sortBy(byArtist, d.val)
-    if (!d.notReverse) byArtist.reverse() 
-
-    byArtist.forEach((d, i) => d.i = i)
-    rowSel.sort((a, b) => a.i - b.i)
-  }
+  d3.values(table).forEach(d => d.updateActive())
 }
 
-function initLongScroll(sel, data, cols){
-  sel.html('')
 
-  var headerSel = sel.append('div.header')
-    .appendMany('div.col', cols)
-    .text(d => d.str)
-    .st({width: d => d.w})
-    .classed('align-right', d => d.ra)
-    .on('click', setSort)
+function initLongScroll(selId, data, cols){
+  var sel = d3.select(selId).html('')
+  var aData = data.slice() // active data
+  var rv = {selId, render, updateActive}
 
-  function setSort(d){
-    d.notReverse = !d.notReverse
+  function addHeader(){
+    var headerSel = sel.append('div.header')
+      .appendMany('div.col', cols)
+      .text(d => d.str)
+      .st({width: d => d.w})
+      .classed('align-right', d => d.ra)
+      .on('click', setSort)
 
-    headerSel.classed('up', 0).classed('down', 0)
-    headerSel.filter(e => e == d).classed(d.notReverse ? 'up' : 'down', 1)
+    function setSort(d){
+      d.notReverse = !d.notReverse
 
-    data = _.sortBy(data, d.val)
-    if (!d.notReverse) data.reverse() 
+      headerSel.classed('up', 0).classed('down', 0)
+      headerSel.filter(e => e == d).classed(d.notReverse ? 'up' : 'down', 1)
 
-    data.forEach((d, i) => d.i = i)
+      aData = _.sortBy(aData, d.val)
+      if (!d.notReverse) aData.reverse() 
 
-    scroll()
+      render()
+    }
   }
+  addHeader()
 
   //https://blocks.roadtolarissa.com/jasondavies/3689677
   //https://dev.to/adamklein/build-your-own-virtual-scroll-part-i-11ib
@@ -119,7 +91,7 @@ function initLongScroll(sel, data, cols){
   var rowHeight = 19
   var contHeight = contSel.node().offsetHeight
   var numRows = 1 + Math.ceil(contHeight/rowHeight)
-  var vData = data.slice(0, numRows) // visable data
+  var vData = aData.slice(0, numRows) // visable data
 
   var currentSel = contSel
     .append('div').st({height: data.length*rowHeight})
@@ -129,14 +101,8 @@ function initLongScroll(sel, data, cols){
     .on('click', function(i){
       var d = vData[i]
       if (!d) return
-
-      var wasSelected = d.selected
-      data.forEach(e => e.selected = d == e)
-      if (wasSelected) d.selected = false
-
-      render()
-      rowSel
-
+      rv.selected = rv.selected != d ? d : null
+      filterAll()
       // TODO play song?
     })
 
@@ -144,23 +110,27 @@ function initLongScroll(sel, data, cols){
     .st({width: d => d.col.w})
     .classed('align-right', d => d.col.ra)
 
+  function updateActive(){
+    aData = data.filter(d => d.active)
+    render()    
+  }
 
   function render(){
     var position = Math.floor(contNode.scrollTop/rowHeight)
     var p0 = d3.clamp(0, position, data.length - numRows)
     var p1 = p0 + numRows
     
-    vData = data.slice(p0, p1)
+    vData = aData.slice(p0, p1)
 
     currentSel.st({transform: `translateY(${p0*rowHeight}px)`})
-    rowSel.classed('selected', i => vData[i] && vData[i].selected)
+    rowSel.classed('selected', i => vData[i] == rv.selected)
     colSel
       .st({opacity: d => vData[d.i] ? 1 : 0})
       .filter(d => vData[d.i])
       .text(d => d.col.val(vData[d.i]))
   }
 
-  return {render}
+  return rv
 }
 
 
