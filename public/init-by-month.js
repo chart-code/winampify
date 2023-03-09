0632307df2b1ce05ee9443d6beaf1a43dbe53d79
Adam Pearce <adampearce@google.com>
2023-03-09 00:00:24 -0500

    tidy

--- public/init-by-month.js
+++ public/init-by-month.js
@@ -3,23 +3,22 @@ window.initByMonth = function(){
 
   var sel = d3.select('.by-month').html('')
 
+  byAlbum.forEach(d => {
+    d.duration = d3.sum(d, e => e.songDuration)
+  })
+
   var byMonthSel = sel.appendMany('div.month', d3.nestBy(byAlbum, d => d.date.slice(0, 7)))
   byMonthSel.append('div.month-key')
     .text(d => d.key)
 
-  var byAlbumSel = byMonthSel.appendMany('div.album', d => _.sortBy(d, d => -d.length))
+  var byAlbumSel = byMonthSel.appendMany('div.album', d => _.sortBy(_.sortBy(d, d => -d.duration), d => -d.length))
     .on('click', playSongs)
   byAlbumSel.append('div.artist').append('span').text(d => d.artist)
   byAlbumSel.append('div.album-title').append('span').text(d => d.keyStr)
   byAlbumSel.append('div.info').append('span')
     .text(d => d.length + (d.length > 1 ? ' tracks' : ' track'))
   byAlbumSel.append('div.info').append('span')
-    .text(d => Math.round(d3.sum(d, e => e.songDuration)/60/1000) + ' min')
-
-
-
-
-  
+    .text(d => Math.round(d.duration/60/1000) + ' min')
 }
 
 if (window.byAlbum) initByMonth()
