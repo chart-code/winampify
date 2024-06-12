3ec0a83d5c47adc5ad07d7fd39f355c1f9ea6908
Zoia Alexanian <peppershredder@gmail.com>
2024-06-11 21:37:07 -0400

    skips artists in the monthly view

--- public/init-by-month.js
+++ public/init-by-month.js
@@ -3,22 +3,29 @@ window.initByMonth = function(){
 
   var sel = d3.select('.by-month').html('')
 
+  var skipArtists = ['DJ Rashad', 'Ty Segall', 'Raffi', 'Randy Newman', 'Weather Report', 'Matthew Dear', '2 Chainz', 'Lil Durk', 'Jimi Hendrix', 'Raffi', 'Future', 'Chief Keef', 'Serengeti', 'Guided By Voices', 'John Williams', 'The Beach Boys']
+  var isSkipArtist = Object.fromEntries(skipArtists.map(artist => [artist, 1]));
   byAlbum.forEach(d => {
     var title = d.keyStr.toLowerCase()
     var hasParenth = title.includes(')')
 
     d.duration = d3.sum(d, e => e.songDuration)/60/1000
     d.isRemix = hasParenth && title.includes('remix')
-    d.isReissue = hasParenth && (title.includes('anniversary') || title.includes('issue') || title.includes('edition') || title.includes('remaster'))
+    d.isReissue = hasParenth && (title.includes('anniversary') || title.includes('issue') || title.includes('edition') || title.includes('remaster')  || title.includes('deluxe'))
     d.isLive = hasParenth && title.includes('live') || title.includes('live from')
     d.isSoundtrack = hasParenth && (title.includes('original') || title.includes('soundtrack'))
     d.isShort = d.duration < 20 && d.length < 5
+    d.isSkipArtist = isSkipArtist[d.artist]
   })
 
 
   var selectedAlbums = byAlbum
-    .filter(d => !d.isRemix && !d.isReissue && !d.isLive && !d.isSoundtrack && !d.isShort)
-    // .filter(d => !d.isShort)
+    .filter(d => !d.isRemix && !d.isReissue && !d.isLive && !d.isSoundtrack && !d.isShort && !d.isSkipArtist)
+
+  // // list artists dominating the monthly list
+  // var byArtist = d3.nestBy(selectedAlbums, d => d.artist)
+  // byArtist = _.sortBy(byArtist, d => d.length)
+  // byArtist.slice(-60).forEach(d => console.log(d[0].artist, d.length))
 
   var byMonthSel = sel.appendMany('div.month', d3.nestBy(selectedAlbums, d => (d.date + '-01').slice(0, 7)))
   byMonthSel.append('div.month-key')
